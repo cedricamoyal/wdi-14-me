39f20cf83fc2d665aab023a08cd761a0a403b711
cedricamoyal <cedric.amoyal@gmail.com>
2016-06-24 15:04:56 +1000

    after project 3

---
+++ 22 D3.js/p3test/sydneyData/main.js
@@ -0,0 +1,481 @@
+
+
+var twitterObject = [
+ {
+   "trends": [
+
+     {
+   "name": "#NBAFinals",
+   "url": "http://twitter.com/search?q=%23NBAFinals",
+   "promoted_content": null,
+   "query": "%23NBAFinals",
+   "tweet_volume": 1797118
+ },
+ {
+   "name": "#GameofThrones",
+   "url": "http://twitter.com/search?q=%23GameofThrones",
+   "promoted_content": null,
+   "query": "%23GameofThrones",
+   "tweet_volume": 796027
+ },
+ {
+   "name": "#MITB",
+   "url": "http://twitter.com/search?q=%23MITB",
+   "promoted_content": null,
+   "query": "%23MITB",
+   "tweet_volume": 458573
+ },
+ {
+   "name": "#WorldRefugeeDay",
+   "url": "http://twitter.com/search?q=%23WorldRefugeeDay",
+   "promoted_content": null,
+   "query": "%23WorldRefugeeDay",
+   "tweet_volume": null
+ },
+ {
+   "name": "#EddieMcGuire",
+   "url": "http://twitter.com/search?q=%23EddieMcGuire",
+   "promoted_content": null,
+   "query": "%23EddieMcGuire",
+   "tweet_volume": null
+ },
+ {
+   "name": "Kabul",
+   "url": "http://twitter.com/search?q=Kabul",
+   "promoted_content": null,
+   "query": "Kabul",
+   "tweet_volume": 35991
+ },
+ {
+   "name": "Corey Parker",
+   "url": "http://twitter.com/search?q=%22Corey+Parker%22",
+   "promoted_content": null,
+   "query": "%22Corey+Parker%22",
+   "tweet_volume": null
+ },
+ {
+   "name": "Kevin Love",
+   "url": "http://twitter.com/search?q=%22Kevin+Love%22",
+   "promoted_content": null,
+   "query": "%22Kevin+Love%22",
+   "tweet_volume": 143209
+ },
+ {
+   "name": "Fake Tradie",
+   "url": "http://twitter.com/search?q=%22Fake+Tradie%22",
+   "promoted_content": null,
+   "query": "%22Fake+Tradie%22",
+   "tweet_volume": null
+ },
+ {
+   "name": "Zane Alchin",
+   "url": "http://twitter.com/search?q=%22Zane+Alchin%22",
+   "promoted_content": null,
+   "query": "%22Zane+Alchin%22",
+   "tweet_volume": null
+ },
+ {
+   "name": "Steph",
+   "url": "http://twitter.com/search?q=Steph",
+   "promoted_content": null,
+   "query": "Steph",
+   "tweet_volume": 632895
+ },
+ {
+   "name": "Pyne",
+   "url": "http://twitter.com/search?q=Pyne",
+   "promoted_content": null,
+   "query": "Pyne",
+   "tweet_volume": null
+ },
+ {
+   "name": "Ambrose",
+   "url": "http://twitter.com/search?q=Ambrose",
+   "promoted_content": null,
+   "query": "Ambrose",
+   "tweet_volume": 98449
+ },
+ {
+   "name": "Draymond",
+   "url": "http://twitter.com/search?q=Draymond",
+   "promoted_content": null,
+   "query": "Draymond",
+   "tweet_volume": 454698
+ },
+ {
+   "name": "Luke Keary",
+   "url": "http://twitter.com/search?q=%22Luke+Keary%22",
+   "promoted_content": null,
+   "query": "%22Luke+Keary%22",
+   "tweet_volume": null
+ },
+ {
+   "name": "Delly",
+   "url": "http://twitter.com/search?q=Delly",
+   "promoted_content": null,
+   "query": "Delly",
+   "tweet_volume": null
+ },
+ {
+   "name": "Jordan",
+   "url": "http://twitter.com/search?q=Jordan",
+   "promoted_content": null,
+   "query": "Jordan",
+   "tweet_volume": 277816
+ },
+ {
+   "name": "The AFL",
+   "url": "http://twitter.com/search?q=%22The+AFL%22",
+   "promoted_content": null,
+   "query": "%22The+AFL%22",
+   "tweet_volume": null
+ },
+ {
+   "name": "Medicare",
+   "url": "http://twitter.com/search?q=Medicare",
+   "promoted_content": null,
+   "query": "Medicare",
+   "tweet_volume": 16438
+ },
+ {
+       "name": "Father's Day",
+       "url": "http://twitter.com/search?q=%22Father%27s+Day%22",
+       "promoted_content": null,
+       "query": "%22Father%27s+Day%22",
+       "tweet_volume": 3097865
+     },
+     {
+       "name": "#agileaus",
+       "url": "http://twitter.com/search?q=%23agileaus",
+       "promoted_content": null,
+       "query": "%23agileaus",
+       "tweet_volume": null
+     },
+     {
+       "name": "#DubNation",
+       "url": "http://twitter.com/search?q=%23DubNation",
+       "promoted_content": null,
+       "query": "%23DubNation",
+       "tweet_volume": 379772
+     },
+     {
+       "name": "#ahpa2016",
+       "url": "http://twitter.com/search?q=%23ahpa2016",
+       "promoted_content": null,
+       "query": "%23ahpa2016",
+       "tweet_volume": null
+     },
+     {
+       "name": "#bcorpadl",
+       "url": "http://twitter.com/search?q=%23bcorpadl",
+       "promoted_content": null,
+       "query": "%23bcorpadl",
+       "tweet_volume": null
+     },
+     {
+       "name": "#pvonewsday",
+       "url": "http://twitter.com/search?q=%23pvonewsday",
+       "promoted_content": null,
+       "query": "%23pvonewsday",
+       "tweet_volume": null
+     },
+     {
+       "name": "#VeranoMTV2016",
+       "url": "http://twitter.com/search?q=%23VeranoMTV2016",
+       "promoted_content": null,
+       "query": "%23VeranoMTV2016",
+       "tweet_volume": 556184
+     },
+     {
+       "name": "#ncec16",
+       "url": "http://twitter.com/search?q=%23ncec16",
+       "promoted_content": null,
+       "query": "%23ncec16",
+       "tweet_volume": null
+     },
+
+{
+       "name": "#mondaymotivation",
+       "url": "http://twitter.com/search?q=%23mondaymotivation",
+       "promoted_content": null,
+       "query": "%23mondaymotivation",
+       "tweet_volume": null
+     },
+     {
+       "name": "#MMVAs",
+       "url": "http://twitter.com/search?q=%23MMVAs",
+       "promoted_content": null,
+       "query": "%23MMVAs",
+       "tweet_volume": 445819
+     },
+     {
+       "name": "#USOpen",
+       "url": "http://twitter.com/search?q=%23USOpen",
+       "promoted_content": null,
+       "query": "%23USOpen",
+       "tweet_volume": 127418
+     },
+     {
+       "name": "#LeMans24",
+       "url": "http://twitter.com/search?q=%23LeMans24",
+       "promoted_content": null,
+       "query": "%23LeMans24",
+       "tweet_volume": 118114
+     },
+     {
+       "name": "#AUSvSA",
+       "url": "http://twitter.com/search?q=%23AUSvSA",
+       "promoted_content": null,
+       "query": "%23AUSvSA",
+       "tweet_volume": null
+     },
+     {
+       "name": "#EuropeGP",
+       "url": "http://twitter.com/search?q=%23EuropeGP",
+       "promoted_content": null,
+       "query": "%23EuropeGP",
+       "tweet_volume": 96876
+     },
+     {
+       "name": "#UFCOttawa",
+       "url": "http://twitter.com/search?q=%23UFCOttawa",
+       "promoted_content": null,
+       "query": "%23UFCOttawa",
+       "tweet_volume": 18903
+     },
+     {
+       "name": "#BabaSiddiqueIftari",
+       "url": "http://twitter.com/search?q=%23BabaSiddiqueIftari",
+       "promoted_content": null,
+       "query": "%23BabaSiddiqueIftari",
+       "tweet_volume": 10483
+     },
+     {
+       "name": "#bigwet",
+       "url": "http://twitter.com/search?q=%23bigwet",
+       "promoted_content": null,
+       "query": "%23bigwet",
+       "tweet_volume": null
+     },
+     {
+       "name": "#nga16",
+       "url": "http://twitter.com/search?q=%23nga16",
+       "promoted_content": null,
+       "query": "%23nga16",
+       "tweet_volume": null
+     },
+     {
+       "name": "#TheVoiceAU",
+       "url": "http://twitter.com/search?q=%23TheVoiceAU",
+       "promoted_content": null,
+       "query": "%23TheVoiceAU",
+       "tweet_volume": null
+     }
+   ],
+   "as_of": "2016-06-20T05:44:58Z",
+   "created_at": "2016-06-20T05:39:01Z",
+   "locations": [
+     {
+       "name": "Sydney",
+       "woeid": 1105779
+     }
+
+     ]
+   }
+   ];
+
+  // var theData = [ 1, 2, 3, 4, 5 ]
+  //
+  // var p = d3.select("body").selectAll("p")
+  //   .data(theData)
+  //   .enter()
+  //   .append("p")
+  //   .text(function (d,i) {
+  //         return "i = " + i + " d = " + d;
+  //    });
+console.log(twitterObject);
+ console.log("The tweets from: " + twitterObject[0].locations[0].name);
+
+var newTwitterArray = [];
+
+   for (var i = 0; i < twitterObject[0].trends.length; i++) {
+    //  console.log(twitterObject[0].trends[i].name);
+    //  console.log(twitterObject[0].trends[i].tweet_volume);
+
+     delete twitterObject[0].trends[i].url;
+     delete twitterObject[0].trends[i].promoted_content;
+     delete twitterObject[0].trends[i].query;
+
+     if (twitterObject[0].trends[i].tweet_volume !== null) {
+        newTwitterArray.push(twitterObject[0].trends[i]);
+     }
+   }
+
+   console.log(twitterObject[0].trends[0]);
+   console.log(twitterObject[0].trends[1]);
+
+   console.log("######");
+
+   console.log(newTwitterArray);
+
+    console.log("######");
+
+    newTwitterArray.sort(function(a, b) {
+    return parseFloat(b.tweet_volume) - parseFloat(a.tweet_volume);
+});
+
+console.log(newTwitterArray);
+
+var maxTweet = newTwitterArray[0].tweet_volume;
+
+console.log("maxTweet = " + maxTweet);
+
+
+
+   d3.select("body").transition()
+       .delay(3000)
+       .each("start", function() { d3.select("body").selectAll("p")
+                                                   .data(newTwitterArray)
+                                                   .enter()
+                                                   .append("p")
+                                                   .text(function (d) {
+                                                         return d.name + " has " + d.tweet_volume + " tweets ";
+                                                    }); })
+
+////////////////////////////////////////////////
+      //
+      var diameter = 600,
+          format = d3.format(",d"),
+          color = d3.scale.category20c();
+
+      var bubble = d3.layout.pack()
+          .sort(null)
+          .size([diameter, diameter])
+          .padding(1.5);
+
+      var svg = d3.select("body").append("svg")
+          .attr("width", diameter*2)
+          .attr("height", diameter)
+          .attr("class", "bubble");
+
+      // var outerWidth = 700;
+      // var outerHeight = 550;
+      // var rMin = 5; // "r" stands for radius
+      // var rMax = 20;
+      // var svg = d3.select("body").append("svg")
+      //   .attr("width", outerWidth)
+      //   .attr("height", outerHeight);
+
+      var rScale = d3.scale.linear().range([20, 200]);
+
+      function render(datass){
+
+        rScale.domain([0, maxTweet]);
+
+        var node = svg.selectAll(".node")
+            .data(datass)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + 600 + "," + 300 + ")"; });
+
+        node.append("circle")
+            .attr("r", function(d) { return rScale( d.tweet_volume ); })
+            .style("fill", function(d) { return color(d.name); })
+
+
+        node.append("text")
+            .attr("dy", ".3em")
+            .style("text-anchor", "middle")
+            .text(function(d) { return d.name; });
+
+}
+
+
+      //   var circles = svg.selectAll("circle").data(datass);
+      //   circles.enter().append("circle")
+      //   .attr("cx", function (d){ return rScale( d.tweet_volume ) * 2; })
+      //   .attr("cy", function (d){ return rScale( d.tweet_volume ) * 4; })
+      //     .attr("r",  function (d){ return rScale( d.tweet_volume ); });
+      //
+      //  var textP = svg.selectAll("text").data(datass);
+      //   textP.enter().append("text")
+      //               .text(function(d) { return d.name; })
+      //               // .style("text-anchor", "middle");
+      //
+      //               .attr("dx", function (d){ return rScale( d.tweet_volume ) * 2; })
+      //               .attr("dy", function (d){ return rScale( d.tweet_volume ) * 4; });
+      //
+      // textP.exit().remove();
+      // }
+
+ // window.setTimeout(render(newTwitterArray), 5000);
+render(newTwitterArray);
+
+/////////////////////////////////////////////////
+
+      var outerWidthB = 1260;
+      var outerHeightB = 700;
+      var margin = { left: 210, top: 30, right: 60, bottom: 110 };
+      var barPadding = 0.2;
+
+      var innerWidth  = outerWidthB  - margin.left - margin.right;
+      var innerHeight = outerHeightB - margin.top  - margin.bottom;
+
+      var svg = d3.select("body").append("svg")
+        .attr("width",  outerWidthB)
+        .attr("height", outerHeightB);
+
+      var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var xAxisG = g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + innerHeight + ")");
+
+      var yAxisG = g.append("g")
+        .attr("class", "y axis");
+
+      var xScale = d3.scale.linear().range(      [0, innerWidth]);
+      var yScale = d3.scale.ordinal().rangeBands([0, innerHeight], barPadding);
+
+      var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
+        .ticks(5)                   // Use approximately 5 ticks marks.
+        .tickFormat(d3.format("s")) // Use intelligent abbreviations, e.g. 5M for 5 Million
+        .outerTickSize(0);          // Turn off the marks at the end of the axis.
+      var yAxis = d3.svg.axis().scale(yScale).orient("left")
+        .outerTickSize(0);          // Turn off the marks at the end of the axis.
+
+      function renderB(data){
+
+        xScale.domain([0, maxTweet]);
+        yScale.domain(data.map( function (d){ return d.name; }));
+
+        xAxisG.call(xAxis);
+        yAxisG.call(yAxis);
+
+        var bars = g.selectAll("rect").data(data);
+        bars.enter().append("rect")
+          .attr("height", yScale.rangeBand());
+
+        bars
+          .attr("x", 0)
+          .attr("y",     function (d){ return yScale(d.name); })
+
+          .attr("width", function (d){ return xScale(d.tweet_volume); })
+          .attr("class", "bars");
+
+            bars.exit().remove();
+
+        var xAxisLabelText = twitterObject[0].locations[0].name;
+        var xAxisLabelOffset = 75;
+
+        var xAxisLabel = xAxisG.append("text")
+          .style("text-anchor", "middle")
+          .attr("x", innerWidth / 2)
+          .attr("y", xAxisLabelOffset)
+          .attr("class", "label")
+          .text(xAxisLabelText);
+
+
+      }
+renderB(newTwitterArray);
