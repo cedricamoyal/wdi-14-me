af6be143c4dde979819ee48c17277b01c8c31440
cedricamoyal <cedric.amoyal@gmail.com>
2016-06-30 22:11:25 +1000

    2.14.1 good

--- 22 D3.js/forcesExample2.14.1/main.js
+++ 22 D3.js/forcesExample2.14.1/main.js
@@ -17,12 +17,12 @@
 		var dataset1 = {
 			nodes:[
 					{name1:"Who",name2:"am I?", color: "#FAF9F4", radius: 90,
-              description: "#"},
+              description: 'I am a French-Australian web developer who became passionate by web development lately. The 12 weeks fulltime course that I just did with General Assembly confirmed everything I thought: I am willing to become a web developer.<br><span class="whoAmIspan">Why did I decide to become a web developer?</span><br>I have been a civil engineer/project manager for the past 8 years. Looking for new challenges, I started to get involved in the start-up community. I even decided to start my own business: Value Your Time (www.valueyourtime.com.au).<br>But after few months I realised that I enjoy developing web solutions much more than every other aspect of the business. I heard about Web Development Bootcamp and decided to go to General Assembly Web Development Immersive Program.<br>That experience confirmed everything I thought: I am willing to become a web developer.'},
 
 	        {name1:"Ex Civil",name2: "Engineer", color: "#52B4AF", radius: 70,
               description: "#"},
 					{name1:"New Web",name2:"Developer", color: "rgb(183, 178, 224)", radius: 70,
-              description: "The WDi Program taught me the skills I need to kickstart my career as a web developer. <br>From programming fundamentals (HTML, CSS, JavaScript, jQuery, AJAX, Ruby) to launching full-stack web apps (Rails, Backbone.js, Bootstrap), I learned to solve problems with code while applying industry best practices in a collaborative environment  (Github, Heroku)."},
+              description: '<span class="whoAmIspan">What did I learn during the WDi program?</span><br>The WDi Program taught me the skills I need to kickstart my career as a web developer.<br>From programming fundamentals (HTML, CSS, JavaScript, jQuery, AJAX, Ruby) to launching full-stack web apps (Rails, Backbone.js, Bootstrap), I learned to solve problems with code while applying industry best practices in a collaborative environment  (Github, Heroku).'},
 
 					{name1:"Project",name2:"Manager", color: "#B6DEDC", radius: 55,
               description: "#"},
@@ -31,14 +31,14 @@
           {name1:"Site",name2:"Engineer", color: "#B6DEDC", radius: 55,
               description: "#"},
 
-          {name1:"P4",name2:"#", color: "rgb(203, 170, 193)", radius: 55,
-              description: "#"},
-          {name1:"P3",name2:"#", color: "rgb(203, 170, 193)", radius: 55,
-              description: "#"},
-          {name1:"P2",name2:"#", color: "rgb(203, 170, 193)", radius: 55,
-              description: 'Travel the world: <br> Link to the application: <a class="webDevAtag" href="https://cedricproject2.herokuapp.com/login " target="_blank">https://cedricproject2.herokuapp.com/login</a> <br>Link to the code: <a class="webDevAtag" href="https://github.com/cedricamoyal/project2cedricheroku " target="_blank">https://github.com/cedricamoyal/project2cedricheroku</a>'},
-          {name1:"P1",name2:"#", color: "rgb(203, 170, 193)", radius: 55,
-              description: 'Tict Tac Toe: <br> Link to the application: <a class="webDevAtag" href="http://cedricamoyal.github.io/Cedricwebsite " target="_blank">http://cedricamoyal.github.io/Cedricwebsite</a> <br>Link to the code: <a class="webDevAtag" href="https://github.com/cedricamoyal/Cedricwebsite " target="_blank">https://github.com/cedricamoyal/Cedricwebsite</a>'},
+          {name1:"Project 4",name2:"Final", color: "rgb(203, 170, 193)", radius: 55,
+              description: 'Link to the application:<br><a class="webDevAtag" href="#" target="_blank">#</a> <br>Link to the code:<br><a class="webDevAtag" href="#" target="_blank">#</a>'},
+          {name1:"Twitter Data",name2:"Visualisation", color: "rgb(203, 170, 193)", radius: 55,
+              description: 'Link to the application:<br><a class="webDevAtag" href="#" target="_blank">#</a> <br>Link to the code:<br><a class="webDevAtag" href="https://github.com/cedricamoyal/project3group " target="_blank">https://github.com/cedricamoyal/project3group</a>'},
+          {name1:"Travel",name2:"The World", color: "rgb(203, 170, 193)", radius: 55,
+              description: 'Link to the application:<br><a class="webDevAtag" href="https://cedricproject2.herokuapp.com/login " target="_blank">https://cedricproject2.herokuapp.com/login</a> <br>Link to the code:<br><a class="webDevAtag" href="https://github.com/cedricamoyal/project2cedricheroku " target="_blank">https://github.com/cedricamoyal/project2cedricheroku</a>'},
+          {name1:"Tic",name2:"Tac Toe", color: "rgb(203, 170, 193)", radius: 55,
+              description: 'Link to the application:<br><a class="webDevAtag" href="http://cedricamoyal.github.io/Cedricwebsite " target="_blank">http://cedricamoyal.github.io/Cedricwebsite</a> <br>Link to the code:<br><a class="webDevAtag" href="https://github.com/cedricamoyal/Cedricwebsite " target="_blank">https://github.com/cedricamoyal/Cedricwebsite</a>'},
 
 			],
 			edges:[
@@ -85,7 +85,7 @@
             .on("click", function (d) {
               $forceBoxText1.empty();
 
-                    var $nameText1 = $("<p>");
+                    var $nameText1 = $("<h3>");
                     $nameText1.html(d.name1 + " " + d.name2);
                     $forceBoxText1.append( $nameText1 );
 
